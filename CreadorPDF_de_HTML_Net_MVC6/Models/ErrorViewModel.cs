3143ef5dc18d46a452749bfe01353714b0de0ee9
CarlosCode502 <carloshl.code21@gmail.com>
2023-10-13 13:09:48 -0600

    Add project files.

---
+++ CreadorPDF_de_HTML_Net_MVC6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CreadorPDF_de_HTML_Net_MVC6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
