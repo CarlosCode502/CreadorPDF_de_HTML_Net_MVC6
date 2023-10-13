97941a9e768407261c870befe13313386820d1fe
CarlosCode502 <carloshl.code21@gmail.com>
2023-10-13 15:43:22 -0600

    Implementación o activación del servicio de DinkToPdf.

---
+++ CreadorPDF_de_HTML_Net_MVC6/Extension/CustomAssemblyLoadContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;//Libreria de la herencia ALContext
+
+namespace CreadorPDF_de_HTML_Net_MVC6.Extension
+{
+    // min 04:30 aprox
+    public class CustomAssemblyLoadContext : AssemblyLoadContext
+    {
+        //Indicamos a nuestro proyecto que vamos a estar trabajando con una libreria de recurso externo
+        //que es la librería .dll
+
+        public IntPtr LoadUnmanagedLibrary(string absolutePath)
+        {
+            return LoadUnmanagedDll(absolutePath);
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            return LoadUnmanagedDllFromPath(unmanagedDllName);
+        }
+
+        protected override Assembly? Load(AssemblyName assemblyName)
+        {
+            //return base.Load(assemblyName);
+            throw new NotImplementedException();
+        }
+    }
+}
--- CreadorPDF_de_HTML_Net_MVC6/Program.cs
+++ CreadorPDF_de_HTML_Net_MVC6/Program.cs
@@ -1,4 +1,24 @@
+//TODOS LOS RECURSOS QUE ESTAMOS UTILIZANDO min 06:00
+using DinkToPdf; //Referencia al paquete de pdf
+using DinkToPdf.Contracts; //Contratos
+using CreadorPDF_de_HTML_Net_MVC6.Extension; //Para poder utilizar la carpeta de extensión
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Utilizando las referencias. min 07:05
+//Implementación o acctivación del servicio de esta libreria
+var context = new CustomAssemblyLoadContext();
+
+//Úbicar el directorio donde se encuentra el archivo y concatenar con la carpeta que contiene la libreria
+context.LoadUnmanagedLibrary(Path.Combine(Directory.GetCurrentDirectory(), "LibreriaPDF\\libwkhtmltox.dll"));
+
+//Utilizamos todas las librerias en un servicio min 8:30
+//IConverter le pertenece a la libreria
+//Sincronizarlo con la configuración del pdf en "(new PdfTools)"
+builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -12,7 +32,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+ 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
